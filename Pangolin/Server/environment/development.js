bfb18b945297b90cc86e341e29d77e815a6fb0d5
santana <johnie_93300@hotmail.fr>
2020-11-17 12:17:08 +0100

    clean repo

--- Pangolin/Server/environment/development.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  dbURl:
-    "mongodb+srv://root:Mvdsap9rlNvr9IBa@cluster0.9ursb.mongodb.net/pangolin?retryWrites=true&w=majority",
-  port: 3000,
-};
--- Pangolin/Server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const authRouter = require("./auth.routes");
-const pangolinRouter = require("./pangolin.routes");
-const { isLoggedIn } = require("..//middlewares/security.middleware");
-
-router.use("/auth", authRouter);
-router.use("/pangolin", isLoggedIn, pangolinRouter);
-
-module.exports = router;
